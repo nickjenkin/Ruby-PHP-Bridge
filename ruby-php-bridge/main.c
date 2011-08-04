4b0662343bef9c3454c4efb3c77f90f657b30496
Nick Jenkin <nick@nickjenkin.com>
2011-08-04 10:43:04 +1000

    Initial Commit

---
+++ ruby-php-bridge/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ruby-php-bridge
+//
+//  Created by Nick Jenkin on 4/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
